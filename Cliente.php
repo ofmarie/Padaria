50eafffee989d7341107449779763b0828f071e8
ofmarie <carpediemmnn@gmail.com>
2026-05-11 16:03:33 -0300

    Refactor Cliente class to remove total attribute

--- Cliente.php
+++ Cliente.php
@@ -1,19 +1,22 @@
 <?php
     class Cliente{
         private $nome;
-        private $total;
         
-        public function __construct($nome, $total){
+        
+        public function __construct($nome){
             $this->nome = $nome;
-            $this->total = $total;
         }  
 
         public function comprar(){
             echo"<p>" . $this->nome . " realizou a compra.";
         }
 
+        public function getNome(){
+            return $this->nome;
+        }
+
         public function __toString(){
-            return $this->nome();
+            return $this->nome;
         }
     }
 ?>
